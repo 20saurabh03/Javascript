41c44894db630a6800c327650e44e40f1d232647
SAURABH SHARMA <78942642+20saurabh03@users.noreply.github.com>
2024-03-11 13:05:24 +0000

    Second Commit

---
+++ 01_basic/02_datavariable.js
@@ -0,0 +1,16 @@
+let name = "saurabh sharma"
+let age =18;
+let isLoggedIn = true;
+
+
+// number =2 to pow 53
+// bigInt 
+// string = ""
+// null => stand alone value 
+// undefined =>
+// symbol => unique ;
+
+
+console.log(typeof name );
+console.log(typeof null ); // object 
+console.log(typeof undefined ); // undefined 
