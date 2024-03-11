9b874416b39148ca74bb75f255a0882639bbe2d5
SAURABH SHARMA <78942642+20saurabh03@users.noreply.github.com>
2024-03-11 14:41:10 +0000

    third Commit

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,52 @@
+let name = true
+
+let userName = Number(name);
+
+// console.log(typeof userName);
+// console.log(userName)
+
+// "33" = 33
+// "33abc" = NaN
+// True= 1, False => 0 
+
+let isboolean = false ;
+let isLoggedIn = Boolean(isboolean)
+// console.log(isboolean)
+
+// 1=> true , 2=>false
+// "" => false, "ashu"=> true
+
+let someNumber = 33;
+let number = String(someNumber);
+// console.log(someNumber);
+// console.log(typeof(number));
+
+
+// -------------------------Operations------------------
+
+let val = 33
+let negVal = - val
+// console.log(negVal)
+
+// console.log(2**3)
+// console.log(2%3)
+
+let n1="saurabh"
+let n2=" sharma "
+
+// console.log(n1+n2);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(2+2 + "3");
+
+
+// console.log((3+4) *5 %3)
+
+let points =1100
+console.log(points++)
+console.log(points++)
+
+
+
---
+++ 01_basic/04_Comprison.js
@@ -0,0 +1,24 @@
+
+// console.log(2>=1);
+// console.log(2!=1)
+// console.log(2==2);
+
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+
+// console.log(undefined> true)
+// console.log(undefined==0)
+// console.log(undefined>=0)
+
+
+// === (also check Datatypes of the number )
+
+console.log("2"=== 2);
+console.log(2 === 2);
+
+
+
+
+
+
